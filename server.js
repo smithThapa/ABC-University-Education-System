7d9dc935e2a2f1e687dffc1b78330e783556811d
Jose Jimenez <jl.jimenezreid@gmail.com>
2019-09-02 11:16:27 +1000

    error fixed

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 process.on('uncaughtException', err => {
   console.log('UNCAUGHT EXCEPTION! Shutting down...');
   console.log(err.name, err.message);
+  console.log(err.stack);
 
   process.exit(1);
 });
