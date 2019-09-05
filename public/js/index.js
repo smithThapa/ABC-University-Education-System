93389c9cfaa71a46115dae3f3735ca11850be232
Smith Thapa <smith.thapa@gmail.com>
2019-09-05 17:37:55 +1000

    work on Thursday

--- public/js/index.js
+++ public/js/index.js
@@ -6,12 +6,20 @@ import { showAlert } from './alerts';
 const loginForm = document.getElementById('login-form');
 const logoutBtn = document.getElementById('logoutBtn');
 
+
+document.getElementById('fileInput').onchange = function() {
+  alert(`Selected file: ${this.value}`);
+};
+
+
+
 if (loginForm) {
   loginForm.addEventListener('submit', e => {
     e.preventDefault();
     const email = document.getElementById('inputEmail').value;
     const password = document.getElementById('inputPassword').value;
     login(email, password);
+ 
   });
 }
 
